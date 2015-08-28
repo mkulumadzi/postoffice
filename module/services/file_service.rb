76d978daa0183612ae1814b11c26a8da1a2ab6e3
Evan Waters <evan.waters@gmail.com>
2015-08-28 12:42:03 -0700

    Removed methods for routing image requests directly to AWS

--- module/services/file_service.rb
+++ module/services/file_service.rb
@@ -37,53 +37,6 @@ module Postoffice
 			cards
 		end
 
-		## Might not need this, if Cloudfront can do the trick...
-		# def self.get_presigner
-		# 	Aws::S3::Presigner.new
-		# end
-		#
-		# def self.get_object key
-		# 	bucket = self.get_bucket
-		# 	Aws::S3::Object bucket, key
-		# end
-		#
-		# def self.add_thumb key, thumb
-		# 	key_split = key.split('.')
-		# 	extension = key_split.pop
-		# 	key_split.join("") + "_" + thumb + extension
-		# end
-		#
-		# def self.thumbnail_exists? key, thumb
-		# 	key = self.add_thumb key, thumb
-		# 	object = self.get_object key
-		# 	object.exists?
-		# end
-		#
-		# def self.create_thumbnail_version_if_necessary key, thumb
-		# 	if !(self.thumbnail_exists? key, thumb)
-		# 		self.create_thumbnail key, thumb
-		# 	end
-		# end
-		#
-		# def self.fetch_image_with_thumb key, thumb
-		# 	Dragonfly.app.fetch(key).thumb(thumb)
-		# end
-		#
-		# def self.create_thumbnail key, thumb
-		#
-		# end
-		#
-		# def self.get_presigned_url key
-		# 	presigner = self.get_presigner
-		# 	presigner.presigned_url(:get_object, bucket: ENV['AWS_BUCKET'], key: key, expires_in: 60)
-		# end
-		#
-		# def self.get_presigned_url_for_object_and_create_thumbnail_version_if_necessary key, thumb = nil
-		# 	if thumb then self.create_thumbnail_version_if_necessary key, thumb end
-		# 	if thumb then key = self.add_thumb key, thumb end
-		# 	self.get_presigned_url key
-		# end
-
 	end
 
 end
